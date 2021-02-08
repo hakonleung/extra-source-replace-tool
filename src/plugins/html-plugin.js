ca42669bb11d73d05bfde0caa5bfbd745cf61fcd
hakonliang <hakonliang@tencent.com>
2021-02-08 16:25:11 +0800

    feat: refactor

--- src/plugins/html-plugin.js
+++ src/plugins/html-plugin.js
@@ -3,14 +3,14 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 // If your plugin is using html-webpack-plugin as an optional dependency
 // you can use https://github.com/tallesl/node-safe-require instead:
 // const HtmlWebpackPlugin = require('safe-require')('html-webpack-plugin')
-const templateTransform = require('../core/template-transform')
+const HtmlTransformer = require('../core/transformer/html')
 
 class HtmlLinkTransformPlugin {
   apply(compiler) {
     compiler.hooks.compilation.tap('HtmlLinkTransformPlugin', (compilation) => {
       HtmlWebpackPlugin.getHooks(compilation).beforeEmit.tapAsync(
         'HtmlLinkTransformPlugin',
-        (data, cb) => templateTransform(data.html).then(html => cb(null, { ...data, html }))
+        (data, cb) => new HtmlTransformer({ code: data.html }).transformAsync().then(html => cb(null, { ...data, html }))
       )
     })
   }
