eccbe24ab19e7989c5e8c9f55527d5a51251589a
hakonliang <hakonliang@tencent.com>
2021-02-23 11:06:49 +0800

    feat: export transformer

--- src/index.js
+++ src/index.js
@@ -2,10 +2,16 @@ const core = require('./core')
 const cssLoader = require('./loaders/css-loader')
 const tsLoader = require('./loaders/ts-loader')
 const HtmlPlugin = require('./plugins/html-plugin')
+const CssTransformer = require('./core/transformer/css')
+const TsTransformer = require('./core/transformer/ts')
+const HtmlTransformer = require('./core/transformer/html')
 
 module.exports = {
   core,
   cssLoader,
   tsLoader,
-  HtmlPlugin
+  HtmlPlugin,
+  CssTransformer,
+  TsTransformer,
+  HtmlTransformer,
 }
